9611f7ff485c8517124c8fad4e94567d08b70757
Jakob Guddas <jakob.guddas@openmailbox.org>
2021-10-31 13:15:29 +0100

    build: cypress only trash screenshots of currently executed test before run

--- cypress/plugins/index.js
+++ cypress/plugins/index.js
@@ -1,7 +1,17 @@
+const trash = import('trash')
 const path = require('path')
 const { startDevServer } = require('@cypress/vite-dev-server')
 
 module.exports = (on, config) => {
+  on('before:run', ({ specs, config }) => {
+    trash.then(i => i.default(specs.map(spec => path.join(
+      config.screenshotsFolder,
+      ...path.relative(
+        config.component.componentFolder,
+        spec.relative
+      ).split(/[\\\/]/g)
+    ))))
+  })
   on('dev-server:start', (options) => {
     return startDevServer({
       options,
